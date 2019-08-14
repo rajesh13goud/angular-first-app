8da27b08a41b62f4aa68cb1d84c240ebf4e3cb94
rajesh13goud <rajeshdgoud@gmail.com>
2019-08-14 14:55:57 +0530

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {ProductListComponent} from  './product-list/product-list.component'
+import {ProductListComponent} from  './product-list/product-list.component';
+
+import {TopBarComponent} from './top-bar-list/top-bar.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,ProductListComponent ],
+  imports:      [ BrowserModule, ReactiveFormsModule,
+  RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
+     ],
+  declarations: [ AppComponent, HelloComponent,ProductListComponent, TopBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
