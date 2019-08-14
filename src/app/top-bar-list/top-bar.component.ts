30a437270b3d581c2fbdd8f9b049969cf46e14b7
rajesh13goud <rajeshdgoud@gmail.com>
2019-08-14 15:04:04 +0530

    4 files modified, 1 added

--- src/app/top-bar-list/top-bar.component.ts
+++ src/app/top-bar-list/top-bar.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
-  templateUrl: './top-bar.component.html'
+  templateUrl: './top-bar.component.html',
+  styleUrls:['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
 
